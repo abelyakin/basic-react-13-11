c148b1cf8c7f8114e70163f9d2cecf419050682b
Alexander Belyakin <alexander.belyakin@agilians.global>
2017-11-22 22:21:16 +0300

    add proptypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ArticleList from './ArticleList'
+import PropTypes from 'prop-types'
 
 function App({ articles }) {
     return (
         <div>
             <h1>App name</h1>
-            <ArticleList articles = {articles} />
+            <ArticleList articles={articles} />
         </div>
     )
 }
+
+App.propTypes = {
+    articles: PropTypes.array
+};
 
 export default App
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,16 +1,21 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Article from './Article'
+import PropTypes from 'prop-types'
 
 export default class ArticleList extends Component {
+    static propTypes = {
+        articles: PropTypes.array
+    }
+
     state = {
         openArticleId: null
     }
 
     render() {
-        const articleElements = this.props.articles.map((article, index) => <li key = {article.id}>
-            <Article article = {article}
-                     isOpen = {this.state.openArticleId === article.id}
-                     toggleOpen = {this.toggleOpenArticle}
+        const articleElements = this.props.articles.map((article, index) => <li key={article.id}>
+            <Article article={article}
+                isOpen={this.state.openArticleId === article.id}
+                toggleOpen={this.toggleOpenArticle}
             />
         </li>)
         return (
@@ -19,12 +24,12 @@ export default class ArticleList extends Component {
             </ul>
         )
     }
-/*
-
-    toggleOpenArticleWitoutCurr(openArticleId) {
-        this.setState({ openArticleId })
-    }
-*/
+    /*
+    
+        toggleOpenArticleWitoutCurr(openArticleId) {
+            this.setState({ openArticleId })
+        }
+    */
 
     toggleOpenArticle = openArticleId => this.setState({ openArticleId })
 }
