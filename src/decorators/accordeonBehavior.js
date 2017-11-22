633364e53011b413de0a4ce8540143b6d6dc2c0e
Alexander Belyakin <alexander.belyakin@agilians.global>
2017-11-22 23:31:56 +0300

    fix closing articles

--- src/decorators/accordeonBehavior.js
+++ src/decorators/accordeonBehavior.js
@@ -10,7 +10,9 @@ export default (OriginalComponent) => class DecoratedComponent extends React.Com
         }
     }
 
-    toggleOpen = (openArticleId) => this.setState({ openArticleId })
+    toggleOpen = (openArticleId) => this.setState({
+        openArticleId: this.state.openArticleId === openArticleId ? null : openArticleId
+    });
 
     render() {
         return <OriginalComponent {...this.props} {...this.state} toggleOpen={this.toggleOpen} />
